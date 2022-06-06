1dcd9a5649b0903f12e5e5a002d15c06b19ab9ad
Abbas <160197415@aston.ac.uk>
2022-06-06 16:36:09 +0100

    Sonarlint bugfixes (commented out line 6 in test file)

--- src/test/java/com/tsi/abbas/gure/program/MyFirstMicroserviceApplicationTests.java
+++ src/test/java/com/tsi/abbas/gure/program/MyFirstMicroserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.tsi.abbas.gure.program;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MyFirstMicroserviceApplicationTests {
 
 	@Test
